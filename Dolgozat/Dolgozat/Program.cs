dacfa300865ebba0c5aaac81dfdcac32d4c7b227
BenceRitzl99 <145541531+BenceRitzl99@users.noreply.github.com>
2024-03-27 11:41:48 +0100

    Add files via upload
    Feltöltés

---
+++ Dolgozat/Dolgozat/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Allatok
+{
+    public string Species;
+    public string Name;
+    public int Age;
+    public double Weight;
+
+    public Allatok(string species, string name, int age, double weight)
+    {
+        Species = species;
+        Name = name;
+        Age = age;
+        Weight = weight;
+
+
+    }
+    static void Allat(string species, string name, int age, double weight)
+    {
+        Console.WriteLine("He/She is a " + species);
+        Console.WriteLine("His/Her name is " + name );
+        Console.WriteLine(age + " Years old");
+        Console.WriteLine(weight +"Kg" );
+    }
+    static void Main(string[] args)
+    {
+        Allat("Pig", "Babe", 5, 45.7);
+    }
+}
+
+
+
+
+
+
+
+
+
